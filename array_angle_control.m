a438258cc43cecd16051aa151a97f4bb3b2aece8
Laetifica <emrys.ap.jarlath@gmail.com>
2022-08-07 20:23:30 -0500

    Add sources, update comments

--- array_angle_control.m
+++ array_angle_control.m
@@ -1,9 +1,12 @@
+% serialport and related functions are from:
+% https://www.mathworks.com/help/matlab/import_export/read-streaming-data-from-arduino.html
 
+% file I/O is from earlier class code by Dr. Markvicka
 
 clear;
 % establish serial connection; replace "COM6" with your com port
 % Check device manager for com ports
-arduinoObj = serialport("COM6",115200)
+arduinoObj = serialport("COM6",115200) % deliberately prints connection
 
 % set terminator characters
 configureTerminator(arduinoObj,"CR/LF");
@@ -19,6 +22,7 @@ homePosition = [000 090 045 020];
 position = homePosition;
 
 %%%%%% Import txt file %%%%%%
+% NUM_ROWS should equal the number of angle sets (rows) your file contains.
 NUM_ROWS = 3; % CONFIGURE THIS TO MATCH YOUR INPUT FILE
 fileID = fopen('angleData.txt');
 A = fscanf(fileID,'%f');
@@ -52,6 +56,13 @@ function [sendString] = encodeString(position)
     % grab a 3-digit number padded with zeroes on the left from each entry
     % in position, and turn it into a 3-character string. Then concatenate
     % the strings and return as sendstring.
+
+    % *Note* this function works fine when negative angles are only two
+    % digits, but may behave erratically when negative 3-digit numbers are
+    % used. Needs testing/upgrade of printf format string (e.g. '%03i').
+    % I suspect this script may need rework to take 4 digits for each angle. 
+    % A better solution would be to process angles to pulselengths in matlab?
+    
     J0 = num2str(position(1), '%03i');
     J1 = num2str(position(2), '%03i');
     J2 = num2str(position(3), '%03i');
